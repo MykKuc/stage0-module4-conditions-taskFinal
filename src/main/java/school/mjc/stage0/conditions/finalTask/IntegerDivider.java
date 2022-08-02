39a41955b113c9a4af5155ac575ec8106e2061cc
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-08-02 16:30:40 +0300

    Almost all completed.

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if(divider == 0){
+            System.out.println("division by zero");
+
+        }
+
+        int dividedValue = dividend/divider;
+        int multipliedResults = dividedValue*divider;
+
+            if(multipliedResults == dividend ) {
+            System.out.println("can be divided completely");
+        }else{
+                if(multipliedResults == 0){
+                    System.out.println("division by zero");
+            }else{
+                    System.out.println("can not de divided");
+                }
+        }
     }
 }
